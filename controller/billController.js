07ba7806f92e2ea62a05bb8daee2cb3e63982ef7
Ar1f007 <muhammadarif9416@yahoo.com>
2022-07-01 03:53:41 +0600

    update delete bills

--- controller/billController.js
+++ controller/billController.js
@@ -11,12 +11,39 @@ exports.addBill = async (req, res) => {
   try {
     const bill = await Bill.create({ name, email, phone, amount });
 
-    console.log(bill);
-
-    return res.status(StatusCodes.OK).json(bill);
+    return res.status(StatusCodes.CREATED).json(bill);
   } catch (error) {
     return res
       .status(StatusCodes.FAILED_DEPENDENCY)
       .json({ message: 'Could not add bill, try again later' });
   }
+};
+
+exports.getBills = async (req, res) => {
+  const bills = await Bill.find().sort({ createdAt: -1 });
+
+  res.status(StatusCodes.OK).json(bills);
+};
+
+exports.updateBill = async (req, res) => {
+  const { id } = req.params;
+
+  const { name, email, phone, amount } = req.body;
+
+  const bill = await Bill.findByIdAndUpdate(id, { name, email, phone, amount }, { new: true });
+  console.log(bill);
+  if (bill) {
+    res.status(StatusCodes.OK).json(bill);
+  }
+};
+
+exports.deleteBill = async (req, res) => {
+  const { id } = req.params;
+  const bill = await Bill.findByIdAndDelete(id).exec();
+
+  if (bill) {
+    return res.status(StatusCodes.OK).json(bill);
+  }
+
+  return res.status(StatusCodes.FAILED_DEPENDENCY).json({ message: 'Could not delete.' });
 };
--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { addBill } = require('../controller/billController');
+const { addBill, getBills, deleteBill, updateBill } = require('../controller/billController');
 const { register, login } = require('../controller/userController');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/add-billing', addBill);
+
+router.get('/billing-list', getBills);
+router.patch('/update-billing/:id', updateBill);
+router.delete('/delete-billing/:id', deleteBill);
 
 module.exports = router;
