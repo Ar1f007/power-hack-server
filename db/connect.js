55219925c829e6d3d082e064626f4684af476820
Ar1f007 <muhammadarif9416@yahoo.com>
2022-07-01 14:31:24 +0600

    changed env variables

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = () =>
-  mongoose.connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ioo2w.mongodb.net/?retryWrites=true&w=majority`
+  );
 
 module.exports = connectDB;
