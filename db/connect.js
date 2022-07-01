1b500fa23c777d6db8ced7ef7e6b5e0332864805
Ar1f007 <muhammadarif9416@yahoo.com>
2022-07-01 14:25:07 +0600

    change mongo uri

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const connectDB = async () =>
-  await mongoose.connect(process.env.MONGO_URI, {
+const connectDB = () =>
+  mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true,
   });
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const connectDB = require('./db/connect');
 const router = require('./routes/route');
-
+const app = express();
 app.use(cors());
 app.use(express.json());
 
